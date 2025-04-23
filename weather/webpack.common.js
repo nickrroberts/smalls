4b3941ffaa81aef6b50ce437b2d98752d6a2a5ee
Nick Roberts <9039456+nickrroberts@users.noreply.github.com>
2025-04-23 14:31:32 -0600

    update public path

--- weather/webpack.common.js
+++ weather/webpack.common.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/smalls/weather/'
+    publicPath: '/smalls/weather/dist/'
   },
   module: {
     rules: [
